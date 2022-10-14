3be364906ff222347c7a4d95cd56089bcc630869
soy-chrislo <green13-16l@hotmail.com>
2022-10-14 08:44:26 -0500

    Adding Lamp and ObliviateInvs libs.

--- src/main/java/com/soychristian/admintools/AdminTools.java
+++ src/main/java/com/soychristian/admintools/AdminTools.java
@@ -2,6 +2,7 @@ package com.soychristian.admintools;
 
 import com.soychristian.admintools.commands.AdminToolsCommand;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AdminTools extends JavaPlugin {
@@ -10,11 +11,21 @@ public final class AdminTools extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getLogger().info("AdminTools has been enabled!");
-        getCommand("admintools").setExecutor(new AdminToolsCommand());
+
+        registerEvents();
+        registerCommands();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void registerCommands(){
+        this.getCommand("admintools").setExecutor(new AdminToolsCommand());
+    }
+
+    public void registerEvents(){
+        PluginManager pm = Bukkit.getPluginManager();
     }
 }
